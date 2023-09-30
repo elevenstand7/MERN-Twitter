991ad3c3317c660f2e2e19dc6af9a2afd14284a1
elevenstand7 <elevenstand7@gmail.com>
2023-09-30 16:11:01 -0400

    Express Server Security

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,49 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// backend/app.js
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const usersRouter = require('./routes/api/users'); // update the import file path
+const tweetsRouter = require('./routes/api/tweets');
+const csrfRouter = require('./routes/api/csrf');
+
+const app = express();
+
+app.use(logger('dev')); // log request components (URL/method) to terminal
+app.use(express.json()); // parse JSON request body
+app.use(express.urlencoded({ extended: false })); // parse urlencoded request body
+app.use(cookieParser()); // parse cookies as an object on req.cookies
+
+// Security Middleware
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express
+    // server will serve the React files statically.)
+    app.use(cors());
+  }
+
+  // Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+    csurf({
+      cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+      }
+    })
+  );
+
+
+// Attach Express routers
+app.use('/api/users', usersRouter); // update the path
+app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
